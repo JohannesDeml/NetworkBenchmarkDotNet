0e28c9a8e6cf75b44369a3d455c8053d25b36341
Johannes Deml <public@deml.io>
2021-02-15 15:43:38 +0100

    Change build before running benchmark for .sh

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Options: https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-build
+# Build targets: https://docs.microsoft.com/en-us/dotnet/core/rid-catalog
+dotnet build --configuration Release --framework net5.0 --output ./bin/NetworkBenchmarkDotNet-Linux/
+
 if [ -z "$1" ]
 then
   mode="All"
