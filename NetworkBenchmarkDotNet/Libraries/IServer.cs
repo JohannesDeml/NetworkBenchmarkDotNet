1209631f4aa0c6235cee30904934a59dd5e6b08b
Johannes Deml <public@deml.io>
2021-02-16 19:34:09 +0100

    Add documentation for IServer

--- NetworkBenchmarkDotNet/Libraries/IServer.cs
+++ NetworkBenchmarkDotNet/Libraries/IServer.cs
@@ -14,11 +14,30 @@ namespace NetworkBenchmark
 {
 	public interface IServer : IDisposable
 	{
+		/// <summary>
+		/// Server is started and ready to receive client connects
+		/// Becomes true after StartServer is called
+		/// </summary>
 		public bool IsStarted { get; }
 
+		/// <summary>
+		/// Start the server and its listening activity
+		/// </summary>
 		public void StartServer();
+
+		/// <summary>
+		/// Start the benchmark. Make every received message count in the statistics
+		/// </summary>
 		public void StartBenchmark();
+
+		/// <summary>
+		/// Stop the benchmark. No more messages are count in the statistics
+		/// </summary>
 		public void StopBenchmark();
+
+		/// <summary>
+		/// Stop the server and its listening activity
+		/// </summary>
 		public void StopServer();
 	}
 }
